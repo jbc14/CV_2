bb323b345794f5d79081fccf976f8e5e0804d2e3
jbc14 <jean.baptiste.conte@gmail.com>
2022-05-06 14:41:30 +0200

    Fin du header

---
+++ script.js
@@ -0,0 +1,13 @@
+// TYPED
+
+let typed = new Typed('.typed', {
+  strings: [`Bonjour à tous ! je m'appel`,`Après dix années dans le commerce, je me lance dans
+  l'aventure passionnante du développement web. Suite à
+  l'obtention d'un diplôme RNCP, je suis à la recherche d'un
+  nouveau défi ! Je saurai mettre créativité, curiosité et
+  autonomie au service de votre projet.`],
+  typeSpeed: 20,
+  backSpeed: 40,
+  smartBackspace: true,
+  loop: false,
+});
